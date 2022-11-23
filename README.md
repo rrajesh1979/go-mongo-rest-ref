be25829c60df7e6b56518f27f0ef637ecb14ba70
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-11-23 18:10:39 -0500

    ✨Adding Swagger Docs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# go-mongo-rest-ref
-🚀Draft of reference project
+# 🚀GoLang MongoDB REST API Reference - URL Shortener
+This is a simple REST API built with GoLang and MongoDB.
+Allows you to create short URLs and redirect to the original URL.
+
+### Swagger Documentation
+Built using https://github.com/swaggo/swag
--- models/url.model.go
+++ models/url.model.go
@@ -7,14 +7,14 @@ import (
 
 type CreateURLRequest struct {
 	LongURL        string    `json:"longURL" bson:"longURL" binding:"required"`
-	ShortUrl       string    `json:"shortURL,omitempty" bson:"shortURL,omitempty"`
+	ShortUrl       string    `json:"shortURL,omitempty" bson:"shortURL,omitempty" swaggerignore:"true"`
 	ExpirationDays int64     `json:"expirationDays,omitempty" bson:"expirationDays,omitempty"`
 	UserID         string    `json:"userID" bson:"userID"  binding:"required"`
-	Status         string    `json:"status,omitempty" bson:"status,omitempty"`
-	Redirects      int64     `json:"redirects,omitempty" bson:"redirects,omitempty"`
-	ExpiresAt      time.Time `json:"expiresAt,omitempty" bson:"expiresAt,omitempty"`
-	CreateAt       time.Time `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
-	UpdatedAt      time.Time `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+	Status         string    `json:"status,omitempty" bson:"status,omitempty" swaggerignore:"true"`
+	Redirects      int64     `json:"redirects,omitempty" bson:"redirects,omitempty" swaggerignore:"true"`
+	ExpiresAt      time.Time `json:"expiresAt,omitempty" bson:"expiresAt,omitempty" swaggerignore:"true"`
+	CreateAt       time.Time `json:"createdAt,omitempty" bson:"createdAt,omitempty" swaggerignore:"true"`
+	UpdatedAt      time.Time `json:"updatedAt,omitempty" bson:"updatedAt,omitempty" swaggerignore:"true"`
 }
 
 type URL struct {
