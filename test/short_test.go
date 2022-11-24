e0bd91ae341e05aeb503ed6ec449e021001ef80f
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-11-23 21:21:20 -0500

    🧪Adding tests

--- test/short_test.go
+++ test/short_test.go
@@ -9,11 +9,6 @@ import (
 const userID = "rrajesh1979"
 
 func TestURLShort(t *testing.T) {
-	//t.Run("Create URL", testCreateURL)
-	//t.Run("Find URL", testFindURL)
-	//t.Run("Find URL by User ID", testFindURLByUserID)
-	//t.Run("Update URL", testUpdateURL)
-	//t.Run("Delete URL", testDeleteURL)
 	longURL := "https://www.google.com"
 	shortURL := utils.GenerateShortLink(longURL, userID)
 	assert.Equal(t, shortURL, "9qcffmSX")
